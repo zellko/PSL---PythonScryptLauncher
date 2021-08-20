c4e1aa5308a974125d2634fd7ed8ffdace8ffaf1
zellko <wyttenda@tutanota.com>
2021-08-20 13:48:41 +0200

    Use subprocess.Popen instead of os.system add command to script conversion in data.py

--- data.py
+++ data.py
@@ -1,5 +1,7 @@
 import pandas
 import os
+import stat
+import pathlib
 
 
 class Data:
@@ -22,13 +24,40 @@ class Data:
         for (index, row) in self.script_data.iterrows():
             print(f"{index + 1}. {row['name']}")
 
-    def add_to_csv(self):
-        """Add a command to the CSV file"""
+    def add_script_to_csv(self):
+        """Add a script to the CSV file"""
         path = input("Enter script path or command: ")
         name = input("Enter the name of the application: ")
+
         if len(path) == 0 or len(name) == 0:
             return "\n--> You need to enter a path/command and a name !\n"
         new_row = {f"path": path, "name": name}
+
+        self.script_data = self.script_data.append(new_row, ignore_index=True)
+        self.update_csv_file()
+        return ""
+
+    def add_cmd_to_csv(self):
+        """Add a command to the CSV file - but we first need convert the command into a scrip"""
+        cmd = input("Enter the command: ")
+        name = input("Enter the name of the application: ")
+
+        if len(cmd) == 0 or len(name) == 0:
+            return "\n--> You need to enter a path/command and a name !\n"
+
+        # Create the script file
+        with open(f"{name}.sh", "w") as script:
+            script.write(f"#!/bin/bash\n{cmd}")
+
+        # Make it executable
+        st = os.stat(f"{name}.sh")
+        os.chmod(f"{name}.sh", st.st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
+
+        # Get the path of directory
+        path = pathlib.Path().resolve()
+        new_row = {f"path": f"{path}/{name}.sh", "name": name}
+
+        # Append the script absolute path and name to the CSV
         self.script_data = self.script_data.append(new_row, ignore_index=True)
         self.update_csv_file()
         return ""
--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from art import logo
 
 
 def clear_screen(print_info=""):
-    os.system('cls' if os.name == 'nt' else 'clear')
+    """Function to 'refresh' the screen"""
+    os.system('cls' if os.name == 'nt' else 'clear')  # Clear the terminal
     print(logo)
     print("\nWelcome to Python Script Launcher")
     data.read_csv()
@@ -14,33 +15,39 @@ def clear_screen(print_info=""):
 
 data = Data()
 clear_screen()
-
 quite_program = False
+
 while not quite_program:
     info = ""
 
     user_input = input(f"Type 1 to {len(data.script_data['name'])} to launch corresponding application."
-                       f"\nType 'add' to add new application, 'remove' to remove application, 'q' to quit ")
+                       f"\nType 'addS' to add new script or 'addC' to add new command\n"
+                       f"Type 'remove' to remove application, 'q' to quit ")
 
     if user_input.lower() == "q":
         quite_program = True
-    elif user_input.lower() == "add":
-        info = data.add_to_csv()
+    elif user_input.lower() == "adds":
+        info = data.add_script_to_csv()
+    elif user_input.lower() == "addc":
+        info = data.add_cmd_to_csv()
     elif user_input.lower() == "remove":
         data.remove_to_csv()
     elif not user_input.isnumeric():
         info = "\n--> Invalid command\n"
     elif int(user_input) <= len(data.script_data["name"]) and len(data.script_data["name"]) > 0:
-        if data.script_data.columns[3] == "gnome":
-            os.system(f'gnome-terminal [-e], -- {data.script_data["path"][int(user_input) - 1]}')
-        else:
-            # TODO Figure out why xterm is closed when python is closed but not gnome
-            os.system(f'xterm -e {data.script_data["path"][int(user_input) - 1]}')
+        # If the user type a valid number, we execute the corresponding script
+        try:
+            command = {data.script_data['path'][int(user_input) - 1]}  # Get the script from the DataFrame
+            subprocess.Popen(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            # stdout and stderr are suppressed to avoid the application printing value on the terminal
+        except FileNotFoundError as error:  # Error if the path is incorrect
+            info = f"\nError: -->{error}<-- when trying to start your application\n"
+        except OSError as error:  # Error if the script is empty or wrong
+            info = f"\nError: -->{error}<-- when trying to start your application\n"
     else:
         info = "\n--> Invalid command\n"
 
     clear_screen(print_info=info)
 
-# TODO Clean Up
-# TODO Add comments
+# TODO Clean Up/Comments
 # TODO Simplify removing process?
