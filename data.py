40e4f3489b5e03e3df96235068aa118694971cb4
zellko <wyttenda@tutanota.com>
2021-08-20 17:29:56 +0200

    Update data.py - Use Try / Except top check if the csv is present or not

--- data.py
+++ data.py
@@ -7,15 +7,11 @@ import pathlib
 class Data:
     def __init__(self):
         """Check if the CSV is present, if not create on"""
-        if not os.path.exists("cmd_path.csv"):
-            terminal = input("Are you on a Gnome environment(Ubuntu, PopOS)? If not, you need to have xterm installed!"
-                             "\nType 'y' or 'n' ").lower()
-            if terminal == "y":
-                dataframe = pandas.DataFrame(columns=["path", "name", "gnome"])
-            else:
-                dataframe = pandas.DataFrame(columns=["path", "name", "xterm"])
+        try:
+            self.script_data = pandas.read_csv("cmd_path.csv")
+        except FileNotFoundError:
+            dataframe = pandas.DataFrame(columns=["path", "name"])
             dataframe.to_csv("cmd_path.csv")
-        self.script_data = pandas.read_csv("cmd_path.csv")
         self.read_csv()
 
     def read_csv(self):
